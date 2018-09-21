d479c606179d4297af962897ea54048c48816e21
2b0x <1315181411@qq.com>
2018-09-22 01:02:27 +0800

    fg

--- moonday/js/main.js
+++ moonday/js/main.js
@@ -295,7 +295,7 @@ function onFrame(event) {
                 //alert()
             }
             getName.onerror = function(msg){
-                //alert('错误了')
+                alert('错误了')
             }
             getName.send()
             //name.innerHtml
@@ -324,6 +324,7 @@ function onFrame(event) {
 
                 var img = new Image()
                 img.src = canvas.toDataURL("image/png")
+
 
                 //为了更换掉上面的页面
                 var svg = document.getElementById('deletedSVG')
