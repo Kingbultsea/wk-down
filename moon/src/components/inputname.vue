f68480558b1ea8fd9125751c95f2a77ab909c77b
2b0x <1315181411@qq.com>
2018-10-08 21:00:56 +0800

    测试版本1

---
+++ moon/src/components/inputname.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="main">
+        <div class="input">
+            <p>请输入你的昵称</p>
+            <input v-model="name"  maxlength="8" limit="6">
+            <div @click="submit">确定</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "inputname",
+        data(){
+            return {
+                name: ''
+            }
+        },
+        methods:{
+            submit(){
+                if(!this.name) this.name = '无名同学'  //小失误 就是if条件判断的时候没有加上this
+                if(this.name){  //又是上面的失误 看来这个情况挺严重的呀
+                    localStorage.setItem('name',this.name)
+                    this.$router.push( {path:'/playing/b'} )
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.main{
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0,0,0,0.5);
+    position: relative;
+    .input{
+        position: absolute;
+        background: rgba(255,255,255,1);
+        width: 80vw;
+        height: 50vw;
+        display:flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        right:50%;
+        transform:translateX(50%);
+        border-radius: 1em;
+        input{
+            background:none;
+            outline:none;
+            border:0px;
+            font-size:1em;
+            border-bottom: 2px #8B572A solid;
+            text-align:center;
+        }
+
+    }
+}
+</style>
---
+++ moon/src/components/tool.js
@@ -0,0 +1,51 @@
+export default class Tool {
+    // 是否为APP
+    static is_cosleep() {
+        return this.is_cosleep_android() || this.is_cosleep_ios()
+    }
+
+    //是否为小睡眠app ios版
+    static is_cosleep_ios() {
+        let ios = false
+        try {
+            ios = typeof
+                window.webkit.messageHandlers.XinchaoApp != 'undefined'
+        } catch (e) {
+            //
+        }
+        return ios
+    }
+
+    //是否为小睡眠app 安卓版
+    static is_cosleep_android() {
+        return typeof window.XinchaoApp != 'undefined'
+    }
+
+    // API请求基地址
+    static get_api_root() {
+        switch (process.env.NODE_ENV) {
+            case 'production':
+                return 'https://api.psy-1.com'
+            case 'test':
+                return 'https://api.debug.psy-1.com'
+        }
+    }
+
+    // WEB跳转基地址
+    static get_web_root() {
+        switch (process.env.NODE_ENV) {
+            case 'production':
+                return 'https://www.heartide.com'
+            case 'test':
+                return 'https://web.debug.psy-1.com'
+        }
+    }
+
+    //加入百度统计
+    static baidu_statics() {
+        var hm = document.createElement('script');
+        hm.src = 'https://hm.baidu.com/hm.js?c6a0be7a2e98e04ca2cf523568e2cbc4';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+    }
+}
