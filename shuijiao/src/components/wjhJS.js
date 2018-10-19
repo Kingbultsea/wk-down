11c113aae52998fdb8c3ef0b28d1623bd6ee6815
2b0x <1315181411@qq.com>
2018-10-20 03:25:36 +0800

    asadda

--- shuijiao/src/components/wjhJS.js
+++ shuijiao/src/components/wjhJS.js
@@ -3,10 +3,47 @@ export default class wjhTool {
 
     }
 
-    promiseForBoolean(boolean){
-        let interval = setInterval()
+    promiseForBoolean(obj = {}){
+        let interval, promise, save = {}
+        Object.assign(save, obj)
+        const func = (resolve)=>{
+            if(obj.boolean === !save.boolean){
+                clearInterval(interval)
+                resolve()
+            }
+        }
+        promise = new Promise((resolve,reject)=>{
+            interval = setInterval(func.bind(null,resolve), 1000)
+        })
+        return promise
+    }
 
+    getAPIData(url, obj = {}, callback, arguments = {}){
+        const api = new new XMLHttpRequest()
+        let paramsString  = ''
+
+        api.open('GET', url)
+
+        api.onreadystatechange = function(){
+            if(this.readyState === 4 && this.status === 200){
+                if(callback) callback()
+            }
+        }
+
+        api.setRequestHeader('Content-Type', 'application/json')
+
+        for(let {val, key} of arguments){
+            paramsString += key + '=' + val + '&'
+        }
+
+        paramsString.replace(/&$/, '')
+
+
+        api.send()
 
     }
+
+
+
 
 }
