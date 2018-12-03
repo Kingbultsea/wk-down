0187edf462ec727e40622ee04fad84350377e1b7
2b0x <1315181411@qq.com>
2018-12-03 06:58:43 -0300

    asdasdasd

--- choujiang1128/precache-manifest.55697d74b05730683ed21def12911ea3.js
+++ choujiang1128/precache-manifest.55697d74b05730683ed21def12911ea3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "872cb8b5f0e6bb93944ba1ead48a145b",
-    "url": "https://webres.psy-1.com/sgy/dist/img/notice.872cb8b5.png"
+    "revision": "5e4ce0b85868124c9d1225c6c629d88d",
+    "url": "https://webres.psy-1.com/sgy/dist/img/prize_bose@2x.5e4ce0b8.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "https://webres.psy-1.com/sgy/dist/robots.txt"
   },
   {
-    "revision": "0760361e3ea6f44e7419800724dcfd06",
-    "url": "https://webres.psy-1.com/sgy/dist/img/prize_default@2x.0760361e.png"
+    "revision": "ec4b8b48759b9fb23254cf16658ac37e",
+    "url": "https://webres.psy-1.com/sgy/dist/img/prize_vip2.ec4b8b48.png"
   },
   {
-    "revision": "24a707fa8b2384107d7e",
-    "url": "https://webres.psy-1.com/sgy/dist/js/app.de6677e5.js"
+    "revision": "cda8285d6fdb66b9e7bc",
+    "url": "https://webres.psy-1.com/sgy/dist/js/app.ed27f904.js"
   },
   {
     "revision": "acf37751e3cbeada34f9",
     "url": "https://webres.psy-1.com/sgy/dist/js/chunk-vendors.abdcff93.js"
   },
   {
-    "revision": "c5bac3a40f72e5142f01b720eca577d9",
-    "url": "https://webres.psy-1.com/sgy/dist/img/go@2x.c5bac3a4.png"
+    "revision": "872cb8b5f0e6bb93944ba1ead48a145b",
+    "url": "https://webres.psy-1.com/sgy/dist/img/notice.872cb8b5.png"
   },
   {
     "revision": "413856e3d99a35f599ef",
@@ -32,19 +32,19 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/img/cloud_bg@2x.4afa5cf6.png"
   },
   {
+    "revision": "0760361e3ea6f44e7419800724dcfd06",
+    "url": "https://webres.psy-1.com/sgy/dist/img/prize_default@2x.0760361e.png"
+  },
+  {
+    "revision": "14a7450b0011a47d7f2977b44669ca9d",
+    "url": "https://webres.psy-1.com/sgy/dist/img/prize_no2.14a7450b.png"
+  },
+  {
     "revision": "b7f3129034cd686d9a4b34837e5d2fe6",
     "url": "https://webres.psy-1.com/sgy/dist/img/prize_muji.b7f31290.png"
   },
   {
-    "revision": "ec4b8b48759b9fb23254cf16658ac37e",
-    "url": "https://webres.psy-1.com/sgy/dist/img/prize_vip2.ec4b8b48.png"
-  },
-  {
-    "revision": "a555b83f7d1fa3e969ad8dda88a3d5a2",
-    "url": "https://webres.psy-1.com/sgy/dist/img/prize_no.a555b83f.png"
-  },
-  {
-    "revision": "09974f8ceccf8f2141234e9a0bcf2033",
+    "revision": "ce4852852cc5374d3fff480025c64870",
     "url": "https://webres.psy-1.com/sgy/dist/index.html"
   },
   {
@@ -52,12 +52,12 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/img/prize_soundbox.a75fa945.png"
   },
   {
-    "revision": "2d160bc3d0802df76583833262c9ccdf",
-    "url": "https://webres.psy-1.com/sgy/dist/img/title@2x.2d160bc3.png"
+    "revision": "a555b83f7d1fa3e969ad8dda88a3d5a2",
+    "url": "https://webres.psy-1.com/sgy/dist/img/prize_no.a555b83f.png"
   },
   {
-    "revision": "14a7450b0011a47d7f2977b44669ca9d",
-    "url": "https://webres.psy-1.com/sgy/dist/img/prize_no2.14a7450b.png"
+    "revision": "2d160bc3d0802df76583833262c9ccdf",
+    "url": "https://webres.psy-1.com/sgy/dist/img/title@2x.2d160bc3.png"
   },
   {
     "revision": "84afc59f63961a4a6752766654a12415",
@@ -72,10 +72,6 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/img/prize_box_bg.ce148efa.png"
   },
   {
-    "revision": "5e4ce0b85868124c9d1225c6c629d88d",
-    "url": "https://webres.psy-1.com/sgy/dist/img/prize_bose@2x.5e4ce0b8.png"
-  },
-  {
     "revision": "cf084d9395bc12dff1a669c69d95eea0",
     "url": "https://webres.psy-1.com/sgy/dist/img/prize_41.cf084d93.png"
   },
@@ -84,11 +80,15 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/img/myprize.240805ff.png"
   },
   {
+    "revision": "c5bac3a40f72e5142f01b720eca577d9",
+    "url": "https://webres.psy-1.com/sgy/dist/img/go@2x.c5bac3a4.png"
+  },
+  {
     "revision": "ca26038a4af429adc43c6c3137785453",
     "url": "https://webres.psy-1.com/sgy/dist/img/explain.ca26038a.png"
   },
   {
-    "revision": "24a707fa8b2384107d7e",
-    "url": "https://webres.psy-1.com/sgy/dist/css/app.30ca4356.css"
+    "revision": "cda8285d6fdb66b9e7bc",
+    "url": "https://webres.psy-1.com/sgy/dist/css/app.33fe7462.css"
   }
 ];
--- choujiang1128/service-worker.js
+++ choujiang1128/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://webres.psy-1.com/sgy/dist/precache-manifest.f96657298fe74a3df24b1e5bb721e73a.js"
+  "https://webres.psy-1.com/sgy/dist/precache-manifest.55697d74b05730683ed21def12911ea3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-common"});
