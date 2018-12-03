46cf0bd07b2008c5df700d06c90c4723e9f86ca7
2b0x <1315181411@qq.com>
2018-12-03 04:28:07 -0300

    上传错了啊

--- choujiang1128/service-worker.js
+++ choujiang1128/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://webres.psy-1.com/sgy/dist/precache-manifest.e610672c36d644d6f323e5e7903acde7.js"
+  "https://webres.psy-1.com/sgy/dist/precache-manifest.1bbc89bd6aaa14ec637fecfb7599ed0c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-common"});
