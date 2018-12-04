cfc8e473f1d5213f71dfd284d96b76dee8a5a8a0
2b0x <1315181411@qq.com>
2018-12-04 13:42:38 -0300

    asdasdasda

--- lotteryCommonPage/precache-manifest.157022d7d8a09e599287cbd74a2edde3.js
+++ lotteryCommonPage/precache-manifest.157022d7d8a09e599287cbd74a2edde3.js
@@ -1,39 +1,47 @@
 self.__precacheManifest = [
   {
-    "revision": "bb5a5624ad9d19092681412ac8332e70",
-    "url": "https://webres.psy-1.com/sgy/dist/fonts/iconfont.bb5a5624.eot"
+    "revision": "47a0ff8b3f947051c0f55681e85b53b2",
+    "url": "https://webres.psy-1.com/sgy/dist/img/Cosleep_Login-in_image_cat2@2x.47a0ff8b.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "https://webres.psy-1.com/sgy/dist/robots.txt"
   },
   {
-    "revision": "6523f0594ad6fa4fdfa11f579db32603",
-    "url": "https://webres.psy-1.com/sgy/dist/img/iconfont.6523f059.svg"
+    "revision": "228e15395054252b8a100ef3b7f19ed8",
+    "url": "https://webres.psy-1.com/sgy/dist/fonts/iconfont.228e1539.ttf"
   },
   {
-    "revision": "cc405353b92c0a6df03e",
-    "url": "https://webres.psy-1.com/sgy/dist/js/app.ee6a7402.js"
+    "revision": "a8246fb85d4c179cecc7",
+    "url": "https://webres.psy-1.com/sgy/dist/js/app.d5599d53.js"
   },
   {
     "revision": "b78bb1c9c65c6a4d6c69",
     "url": "https://webres.psy-1.com/sgy/dist/js/chunk-vendors.c375141d.js"
   },
   {
-    "revision": "15b0f673a6c6ee531b6a0df0b23c0caa",
-    "url": "https://webres.psy-1.com/sgy/dist/img/cosleep_window_img_fail@2x.15b0f673.png"
+    "revision": "bb5a5624ad9d19092681412ac8332e70",
+    "url": "https://webres.psy-1.com/sgy/dist/fonts/iconfont.bb5a5624.eot"
   },
   {
     "revision": "d365addf498a375b35f7",
     "url": "https://webres.psy-1.com/sgy/dist/js/about.e3532e52.js"
   },
   {
-    "revision": "04f20f9c13a2cb4d667f66e8567c5bf1",
+    "revision": "9576838103b481e040a0bca49fe2c3e9",
     "url": "https://webres.psy-1.com/sgy/dist/index.html"
+  },
+  {
+    "revision": "6523f0594ad6fa4fdfa11f579db32603",
+    "url": "https://webres.psy-1.com/sgy/dist/img/iconfont.6523f059.svg"
   },
   {
     "revision": "7da7fc491809d838646773a92fb305a6",
     "url": "https://webres.psy-1.com/sgy/dist/img/cosleep_window_img_login@2x.7da7fc49.png"
+  },
+  {
+    "revision": "15b0f673a6c6ee531b6a0df0b23c0caa",
+    "url": "https://webres.psy-1.com/sgy/dist/img/cosleep_window_img_fail@2x.15b0f673.png"
   },
   {
     "revision": "e225be0255664864ff3bdd2c056f882b",
@@ -44,10 +52,6 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/img/cosleep_home_bg_forest@2x.fe362fc7.png"
   },
   {
-    "revision": "47a0ff8b3f947051c0f55681e85b53b2",
-    "url": "https://webres.psy-1.com/sgy/dist/img/Cosleep_Login-in_image_cat2@2x.47a0ff8b.png"
-  },
-  {
     "revision": "418d488127259273ce2fcc28f5094e57",
     "url": "https://webres.psy-1.com/sgy/dist/img/Cosleep_Login-in_image_cat1@2x.418d4881.png"
   },
@@ -56,11 +60,7 @@ self.__precacheManifest = [
     "url": "https://webres.psy-1.com/sgy/dist/fonts/iconfont.32e9154a.woff"
   },
   {
-    "revision": "228e15395054252b8a100ef3b7f19ed8",
-    "url": "https://webres.psy-1.com/sgy/dist/fonts/iconfont.228e1539.ttf"
-  },
-  {
-    "revision": "cc405353b92c0a6df03e",
-    "url": "https://webres.psy-1.com/sgy/dist/css/app.4d7b6e44.css"
+    "revision": "a8246fb85d4c179cecc7",
+    "url": "https://webres.psy-1.com/sgy/dist/css/app.36cb9511.css"
   }
 ];
