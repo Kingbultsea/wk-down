076a7bee34687a3b06c40ea6af7be35e01ee0323
2b0x <1315181411@qq.com>
2019-01-07 12:14:18 +0800

    init

---
+++ wxPublicShare/js/Parse2XML.js
@@ -0,0 +1,40 @@
+class Parse2XML {
+    constructor (fromUser) {
+        this.fromUser = fromUser
+    }
+
+    message (toUser, mesg = '', createTime = null) {
+        const timeStamp = createTime || Math.floor(Date.now() / 1000)
+        return '<xml>'+
+                    '<ToUserName><![CDATA['+ toUser +']]></ToUserName>' +
+                    '<FromUserName><![CDATA['+ this.fromUser +']]></FromUserName>' +
+                    '<CreateTime>'+timeStamp+'</CreateTime>' +
+                    '<MsgType><![CDATA[text]]></MsgType>' +
+                    '<Content><![CDATA['+ mesg +']]></Content>' +
+                '</xml>'
+    }
+
+    messageAndPic (toUser, list = [{title: '', desc: '', picUrl: ''},{}], createTime = null) {
+        const timeStamp = createTime || Math.floor(Date.now() / 1000) // 这里我也想封装起来... 因为万一错了呢？ 岂不是浪费几分钟维护？ 但是好像需要外部传才好.. emm...
+        let itemsData = null
+        for (let i of list) {
+            itemsData += `<item>
+                              <Title>< ![CDATA[${i.title}] ]></Title>
+                              <Description>< ![CDATA[${i.desc}] ]></Description>
+                              <PicUrl>< ![CDATA[${i.picUrl}] ]></PicUrl>
+                          </item>`.trim()
+        }
+        return `<xml>
+                    <ToUserName>< ![CDATA[${toUser}] ]></ToUserName>
+                    <FromUserName>< ![CDATA[${this.fromUser}] ]></FromUserName>
+                    <CreateTime>${timeStamp}</CreateTime>
+                    <MsgType>< ![CDATA[news] ]></MsgType>
+                    <ArticleCount>1</ArticleCount>
+                    <Articles>
+                        ${itemsData}
+                    </Articles>
+                </xml>`.replace(/(\s*)/g, '')
+    }
+}
+
+module.exports = Parse2XML
