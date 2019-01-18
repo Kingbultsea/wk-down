377db6133b02a7728eb7c86e749232dc74c097c4
2b0x <1315181411@qq.com>
2019-01-18 18:44:35 +0800

    dist

---
+++ exchange/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  baseUrl: 'https://webres.psy-1.com/sgy/dist', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
+  assetsDir: './',
+  pwa: {
+    name: '小睡眠抽奖',
+    themeColor: 'black',
+    manifestPath: 'manifest.json',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
