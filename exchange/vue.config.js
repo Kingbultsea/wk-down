b816042d48bdf1381c0fe1f72178ea896b8ac861
2b0x <1315181411@qq.com>
2019-01-19 17:22:10 +0800

    是

--- exchange/vue.config.js
+++ exchange/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: './', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
+  baseUrl: 'https://webres.psy-1.com/sgy/dist', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
   assetsDir: './',
   pwa: {
     name: '小睡眠抽奖',
