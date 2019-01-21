d61f2b5c71cd42128f9ba2163e2ddbc6c94c6c6c
2b0x <1315181411@qq.com>
2019-01-21 12:09:14 +0800

    提交下所有代码

--- exchange/vue.config.js
+++ exchange/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  baseUrl: 'https://webres.psy-1.com/sgy/dist', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
+  baseUrl: './', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
   assetsDir: './',
   pwa: {
-    name: '小睡眠抽奖',
+    name: '小睡眠会员资格免费领',
     themeColor: 'black',
     manifestPath: 'manifest.json',
     appleMobileWebAppCapable: 'yes',
