a4f4fd940aad14bdd6d7c585711bf578c18d2e68
2b0x <1315181411@qq.com>
2019-01-24 17:52:26 +0800

    根本就没有重复好吧？

--- vipRedemption/src/views/Home.vue
+++ vipRedemption/src/views/Home.vue
@@ -52,6 +52,7 @@ export default {
         console.log(res.data)
         this.$children[0].close = true
         if (res.data.msg.indexOf('重复')) {
+          console.log(res.data.msg.indexOf('重复'))
           console.log('重复了')
           this.$children[0].changeRepeat(res.data.msg)
           return
