5c7d04cdd23cd67daed1ad5cc083c2075b3852d1
2b0x <1315181411@qq.com>
2019-01-24 17:42:05 +0800

    加一个参数就空白？

--- vipRedemption/src/registerServiceWorker.js
+++ vipRedemption/src/registerServiceWorker.js
@@ -19,8 +19,9 @@ if (process.env.NODE_ENV === 'production') {
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
     },
-    error (error) {
-      console.error('Error during service worker registration:', error)
+    error () {
+      console.log('er')
+      // console.error('Error during service worker registration:', error)
     }
   })
 }
