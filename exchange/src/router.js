4e0a030996a9576c877a49b9b6c8371f08ff764b
2b0x <1315181411@qq.com>
2019-03-14 17:07:17 +0800

    9 --> 15 超脑配套知识

--- exchange/src/router.js
+++ exchange/src/router.js
@@ -14,7 +14,7 @@ export default new Router({
             component: Home
         },
         {
-            path: '/editor',
+            path: '/c',
             name: 'wangeditor',
             component: WangEditor
         },
