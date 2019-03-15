4a7e31326fc402e47c42d7b48f39ad78ec47e18f
2b0x <1315181411@qq.com>
2019-03-15 18:17:59 +0800

    ceshifu

---
+++ huawei-join/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div class="app-template">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      a: ''
+    }
+  },
+  methods: {
+    test () {
+      console.log('?????')
+    }
+  },
+  mounted () {
+    this.test()
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    position: relative;
+    overflow: hidden;
+    width: 100vw;
+    height:100vh;
+    font-size: px2html(12px);
+    background-color: RGBA(71, 75, 121, 1);
+    .app-template {
+      width: 100vw;
+      height: 100vh;
+      overflow: scroll;
+    }
+  }
+</style>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  a,a:link,a:visited,a:hover,a:active{
+    text-decoration: none;
+    color:inherit;
+  }
+</style>
---
+++ huawei-join/src/views/Success.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="success">
+    <div class="title">报名成功</div>
+    <div class="desc">请添加微信进入报名审核群审核成功后我们将 会在3月21日前通过电话或微信（yoning-wu） 的方式进行通知</div>
+    <img class="QRblock" src="../assets/二维码.png">
+    <div class="more">更多详情</div>
+    <img class="bg" src="../assets/背景.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'success'
+}
+</script>
+
+<style lang="scss" scoped>
+  .success {
+    width: px2html(375px);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    .title {
+      padding-top: px2html(56px);
+      color: rgba(243, 169, 81, 1);
+      font-size: px2html(30px);
+      font-weight: 900;
+      position: relative;
+      z-index: 2;
+    }
+    .desc {
+      text-align: justify;
+      width: px2html(273px);
+      padding-top: px2html(30px);
+      font-size: px2html(13px);
+      color: rgba(255, 255, 255, 1);
+      position: relative;
+      z-index: 2;
+    }
+    .QRblock {
+      padding-top: px2html(50px);
+      width: px2html(154px);
+      position: relative;
+      z-index: 2;
+    }
+    .more {
+      position: relative;
+      z-index: 2;
+      margin-top: px2html(43px);
+      color: #fff;
+      width: px2html(146px);
+      background-color: rgba(243, 169, 81, 1);
+      height: px2html(35px);
+      border-radius: px2html(18px);
+      font-weight: 500;
+      font-size: px2html(18px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .bg {
+      left: 0;
+      bottom: 0;
+      position: absolute;
+      width: px2html(375px);
+      z-index: 1;
+    }
+  }
+</style>
