992a029409e9ff4a03e5ad3101d7b2ee94c1d8df
2b0x <1315181411@qq.com>
2019-04-11 15:48:53 +0800

    华为优惠活动

---
+++ huawei-youhui/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="app-template">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Share from '@/js/shareAndGetName'
+import '../node_modules/animate.css/animate.css'
+
+export default {
+  data () {
+    return {
+      a: '',
+      url: 'https://api.debug.psy-1.com'
+    }
+  },
+  methods: {
+    test (desc) {
+      const share = new Share({ pic: 'http://wx1.sinaimg.cn/large/006Zdy2vgy1g0fd9no3cyj305f05fmwz.jpg', url: window.location.href.split('#')[0], title: '嘘~悄悄把心告诉你', desc: desc })
+      share.appShare()
+      share.rawWeiXinShare(this.url)
+    }
+  },
+  mounted () {
+    this.test()
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    position: relative;
+    overflow: hidden;
+    width: 100vw;
+    height:100vh;
+    font-size: px2html(12px);
+    background-color: #2642c6;
+    .app-template {
+      width: 100vw;
+      height: 100vh;
+      overflow: scroll;
+    }
+  }
+</style>
+
+<style lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  a,a:link,a:visited,a:hover,a:active{
+    text-decoration: none;
+    color:inherit;
+  }
+</style>
---
+++ huawei-youhui/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home" :style="home_height">
+    <img class="ig" src="../assets/po.jpg" />
+    <div @click="oo" class="open-remeber"></div>
+    <div @click="ooo" class="get-in"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      home_height: 0
+    }
+  },
+  components: {
+  },
+  methods: {
+    oo () {
+      window.location = 'https://www.heartide.com/app/redirect?code=10017&goods_id=4'
+    },
+    ooo () {
+      window.location = 'https://www.heartide.com/app/redirect?code=10025&goods_id=16'
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+  .home {
+    position: relative;
+    width: px2html(375px);
+    >.ig {
+      width: 100vw;
+    }
+    >.open-remeber {
+      position: absolute;
+      top: px2html(580px);
+      width: px2html(140px);
+      height: px2html(30px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+    >.get-in {
+      position: absolute;
+      top: px2html(870px);
+      width: px2html(140px);
+      height: px2html(30px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+  }
+</style>
