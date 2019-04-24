a1192b25b31392990e217ac0609dd25d27faa8ec
2b0x <1315181411@qq.com>
2019-04-24 21:34:43 +0800

    z

--- huawei-youhui/src/router.js
+++ huawei-youhui/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Xm from './views/限免.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/xm',
+      name: 'xm',
+      component: Xm
     },
     {
       path: '/about',
---
+++ huawei-youhui/src/views/限免.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="xm" :style="home_height">
+    <img class="ig" src="../assets/H5限时免费.jpg" />
+    <div @click="oo" class="open-remeber"></div>
+    <div @click="ooo" class="get-in"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'xm',
+  data () {
+    return {
+      home_height: 0
+    }
+  },
+  components: {
+  },
+  methods: {
+    oo () {
+      window.location = 'https://www.heartide.com/app/redirect?code=10017&goods_id=4'
+    },
+    ooo () {
+      window.location = 'https://www.heartide.com/app/redirect?code=10025&goods_id=16'
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+  .xm {
+    position: relative;
+    width: px2html(375px);
+    background-color: #fff8ef;
+    >.ig {
+      width: 100vw;
+    }
+    >.open-remeber {
+      position: absolute;
+      top: px2html(585px);
+      width: px2html(170px);
+      height: px2html(40px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+    >.get-in {
+      position: absolute;
+      top: px2html(890px);
+      width: px2html(170px);
+      height: px2html(40px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+  }
+</style>
