099db20a3444adffebe39ab62392bdd89fc73ed9
2b0x <1315181411@qq.com>
2019-06-27 11:56:25 +0800

    z

--- exchange/vue.config.js
+++ exchange/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: 'https://webres.psy-1.com/sgy/dist', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
+  baseUrl: process.env.NODE_ENV === 'production' ? 'https://webres.psy-1.com/sgy/dist' : '', // /cosleep/teacher/    ./ https://webres.psy-1.com/sgy/dist
   assetsDir: './',
   pwa: {
     name: '小睡眠会员资格免费领',
