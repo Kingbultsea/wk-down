948af865e524ac6d87fae8cf37c165a321cc7f60
2b0x <1315181411@qq.com>
2019-07-25 21:42:41 +0800

    z

--- zdds/src/page/center.vue
+++ zdds/src/page/center.vue
@@ -197,7 +197,7 @@ export default {
                   one: '',
                   two: ''
               },
-              sixteen
+              sixteen: ''
               },
           dsb:'',
           A:'',
