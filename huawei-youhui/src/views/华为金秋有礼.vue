b6e87aac37ef08ff9e301cfdd7fd50590ed99f48
wjh123 <894275157@qq.com>
2019-11-04 17:16:45 +0800

    db

---
+++ huawei-youhui/src/views/华为金秋有礼.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="hwcjdefl" :style="home_height">
+    <img class="ig" src="../assets/华为-H5(3).jpg" />
+    <div @click="oo" class="a a1"></div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+/* eslint-disable */
+export default {
+  name: 'smbwj',
+  data () {
+    return {
+      home_height: 0
+    }
+  },
+  components: {
+  },
+  methods: {
+    // https://www.heartide.com/app/redirect?code=10017&goods_id=4#/
+    callAppRouter (method, params={}, callback) {
+      let req = {
+        'Method': method,
+        'Data': params
+      }, cbName = `CB_${Date.now()}_${Math.ceil(Math.random() * 10)}`
+      req = JSON.stringify(req)
+      Xinchao.Web[cbName] = (err, result) => {
+        if (callback) {
+          callback(err, result)
+        }
+        delete Xinchao.Web[cbName]
+      }
+      if (/(iPhone|iPad|iPod)/i.test(navigator.userAgent)) {
+        window.webkit.messageHandlers.XinchaoApp.postMessage({
+          req,
+          cbName
+        })
+      } else {
+        XinchaoApp.callRouter(req, cbName)
+      }
+    },
+    oo () {
+      _hmt.push(['_trackEvent', 'nav', 'hw3', `574`])
+      let pre = {
+        'code': 10017,
+        'goods_id': 4
+      }
+      this.callAppRouter('Redirect', pre, function(err, result){})
+    },
+    ooo () {
+      window.location = 'https://www.heartide.com/app/redirect?code=10025&goods_id=16'
+    }
+  },
+  mounted () {
+    document.title = '超级大额返利'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .hwcjdefl {
+    position: relative;
+    width: px2html(375px);
+    background-color: #fff8ef;
+    height: max-content;
+    >.ig {
+      width: 100vw;
+      margin-bottom: px2html(-10px);
+    }
+    >.open-remeber {
+      position: absolute;
+      top: px2html(385px);
+      width: px2html(170px);
+      height: px2html(40px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+    >.get-in {
+      position: absolute;
+      top: px2html(715px);
+      width: px2html(170px);
+      height: px2html(40px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+    >.a {
+      position: absolute;
+      width: px2html(170px);
+      height: px2html(40px);
+      right: 50%;
+      transform: translateX(50%);
+    }
+    >.a1 {
+      top: px2html(585px);
+    }
+    >.a2 {
+      top: px2html(760px);
+    }
+    >.a3 {
+      top: px2html(1085px);
+    }
+    >.a4 {
+      top: px2html(1385px);
+    }
+  }
+</style>
